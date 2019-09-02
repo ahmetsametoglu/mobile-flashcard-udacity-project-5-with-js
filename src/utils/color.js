98489def9bc58a8df304f7c285e9fca677cb384d
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-09-02 22:57:32 +0200

    feat: colors created

---
+++ src/utils/color.js
@@ -0,0 +1,14 @@
+export const Colors = {
+  primaryColor: 'darkviolet',
+  secondaryColor: 'darkgoldenrod',
+  thirdColor: 'navajowhite',
+  successColor: 'green',
+  errorColor: 'red',
+  warningColor: 'yellow',
+  grey: 'grey',
+  white: 'white',
+  darkgoldenrod: 'darkgoldenrod',
+  black: 'black',
+  red: 'red',
+  loadingBackRoundColor: 'rgba(246, 246, 235, 0.8)',
+};
