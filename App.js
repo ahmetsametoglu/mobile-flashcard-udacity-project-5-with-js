4fb2aa0e170066839bf6f7d039821789209d4fc0
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-09-02 23:11:21 +0200

    feat: context-provider and navigation implemented to app.js

--- App.js
+++ App.js
@@ -1,19 +1,34 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {View, StatusBar} from 'react-native';
 
-export default function App() {
+import Constants from 'expo-constants';
+import {Colors} from './src/utils/color';
+import {NotificationHelper} from './src/utils/helper';
+import StackNav from './src/navigators/stack-nav';
+import Loading from './src/hoc/loading';
+import StoreContextProvider from './src/store/store-context';
+
+const App = () => {
+  console.log('[App]: init');
+
+  NotificationHelper.setLocalNotification();
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <StoreContextProvider>
+      <View style={{flex: 1}}>
+        <View
+          style={{
+            backgroundColor: Colors.thirdColor,
+            height: Constants.statusBarHeight,
+          }}>
+          <StatusBar translucent backgroundColor={Colors.thirdColor} barStyle="light-content" />
+        </View>
+        <Loading>
+          <StackNav />
+        </Loading>
+      </View>
+    </StoreContextProvider>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
