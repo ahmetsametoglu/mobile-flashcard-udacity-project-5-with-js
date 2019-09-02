0dc5dcc6366b46d23d6f42a778dd03fef32960b4
Ahmet <ahmetsametoglu.fr@gmail.com>
2019-09-02 22:58:07 +0200

    feat: action-type created

---
+++ src/store/action-type.js
@@ -0,0 +1,9 @@
+export const ActionType = {
+  FetchDeckList: 'FetchDeckList',
+  AddDeckToList: 'AddDeckToList',
+  RemoveDeck: 'RemoveDeck',
+  AddCardToDeck: 'AddCardToDeck',
+  RemoveCard: 'RemoveCard',
+  ShowLoading: 'ShowLoading',
+  HideLoading: 'HideLoading',
+};
